65a85640fcc34771f0f0b50ff78419028759b3c2
Patricia <pmd8@ua.pt>
2022-05-02 14:22:52 +0100

    check sonarqube2

--- HW1/coviddata/src/test/java/ua/tqs/coviddata/ControllerTests/CovidControllerRestAssuredTest.java
+++ HW1/coviddata/src/test/java/ua/tqs/coviddata/ControllerTests/CovidControllerRestAssuredTest.java
@@ -3,9 +3,7 @@ package ua.tqs.coviddata.ControllerTests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import ua.tqs.coviddata.Controller.CovidDataController;
-@WebMvcTest(CovidDataController.class)
+
 class CovidControllerRestAssuredTest {
     @Test
     void assert1(){
